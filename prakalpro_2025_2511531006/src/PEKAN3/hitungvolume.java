a2d0ee02b1604d82b41fc8734567a5873ab150be
lexi210919-dotcom <lexi210919@gmail.com>
2025-09-27 20:57:27 +0700

    Add files via upload

---
+++ prakalpro_2025_2511531006/src/PEKAN3/hitungvolume.java
@@ -0,0 +1,59 @@
+package PEKAN3;
+import java.util.Scanner;
+public class hitungvolume {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Inisialisasi a
+	    double p = 3.14	;  
+	    double v;
+	    int r,t;
+	    Scanner a = new Scanner(System.in);
+	    System.out.print("Masukkan jari jari tabung : ");
+	    r = a.nextInt();
+	    System.out.print("Masukkan tinggi tabung");
+	    t = a.nextInt();
+	    a.close();
+	    v = p * r * r * t; 
+	    System.out.println("Volume tabung : " + v);
+	    
+	    
+	    
+	    	
+	    
+	    
+	    
+	    
+	   
+	    
+	    
+	    
+	    
+	    		
+	    
+	    
+	    
+	    
+	   
+	    
+	    
+	   
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    
+	}
+
+}
