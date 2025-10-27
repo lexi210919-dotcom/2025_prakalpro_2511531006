90904e7206c9d1e94b18d3bbb34c0c264cf24545
Tensoraki <zakiiialhafizz@gmail.com>
2025-10-27 15:56:15 +0700

    pekan5

--- prakalpro_2025_2511531006/src/pekan5/nestedFor0.java
+++ prakalpro_2025_2511531006/src/pekan5/nestedFor0.java
@@ -1,9 +1,19 @@
 
 public class nestedFor0 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void main(String[] args) {
 
-	}
+		        for (int line = 1; line <= 5; line++) {
+		            
+		            for (int j = 1; j <= (-1 * line + 5); j++) {
+		                System.out.print(".");
+		            }
+		            
+		            System.out.print(line);
+		            System.out.println();
+		        }
+		        
+		    }
+		}
+		
 
-}
---
+++ prakalpro_2025_2511531006/src/pekan5/nestedFor1.java
@@ -0,0 +1,14 @@
+
+public class nestedFor1 {
+
+	public static void main(String[] args) {
+		int j;
+		for (int i= 1; i <= 5; i++);
+		   for (int j1 = 1; j1 <=5; j1++);
+		       System.out.print("*");
+	    }    
+	    System.out.println();
+       }// to end the line
+	
+
+
