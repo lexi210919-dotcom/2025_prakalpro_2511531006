f3bb3fb84ddd7311a42113a46840adb5e667f61b
lexi210919-dotcom <lexi210919@gmail.com>
2025-11-16 21:46:45 +0700

    Add files via upload

---
+++ prakalpro_2025_2511531006/src/Pekan7_2511531006/TugasAlproPekan7_2511531006.java
@@ -0,0 +1,57 @@
+package Pekan7_2511531006;
+
+import java.util.Scanner;
+
+public class TugasAlproPekan7_2511531006 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        System.out.println("===== REGISTRASI AKUN BARU =====");
+        System.out.print("Masukkan Username: ");
+        String username = input.nextLine();
+
+        System.out.print("Masukkan Password: ");
+        String password = input.nextLine();
+
+        System.out.print("Masukkan Email: ");
+        String email = input.nextLine();
+
+        System.out.print("Masukkan Pin (6 digit): ");
+        int pinAngka = input.nextInt();
+
+        Akun akun = new Akun(email, email, email, pinAngka);
+
+        if (!akun.isEmailValid()) {
+            System.out.println("\n--- REGISTRASI GAGAL ---");
+            System.out.println("Email Anda '" + akun.getEmail() + "' tidak valid (harus mengandung '@' dan '.').");
+            System.out.println("Silakan coba lagi.");
+        } else if (!akun.isPasswordValid()) {
+            System.out.println("\n--- REGISTRASI GAGAL ---");
+            System.out.println("Password tidak valid (minimal 8 karakter). Silakan coba lagi.");
+        } else {
+        	    System.out.println("\n--- REGISTRASI BERHASIL ---");
+        	    System.out.println("Akun untuk '" + akun.getUsername() + "' telah berhasil dibuat.\n");
+        	    System.out.println("== Detail Akun ==");
+        	    System.out.println("Username (lowercase): " + akun.getUsername().toLowerCase());
+        	    System.out.println("Email (Uppercase): " + akun.getEmail().toUpperCase());
+        	    System.out.println("ID Pengguna (Gabungan): " + akun.getUsername() + akun.getEmail());
+        	    System.out.println("\n=== Uji Tipe Data (PIN Anda: " + akun.getPinAngka() + ") ==");
+        	    System.out.println("PIN (int) * 10 = " + (akun.getPinAngka() * 10));
+        	}
+
+     
+
+}
+  
+
+
+
+
+	
+
+}
+
+
+
+
